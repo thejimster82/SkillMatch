2dbf80ac04cfaa8f6171113c0ec3e2dcbfa33f54
coldbuffalo <coldbuffalo0@gmail.com>
2019-02-13 16:44:41 -0500

    added home template, google signin mostly good

--- skillmatch/matching/views.py
+++ skillmatch/matching/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
--- skillmatch/skillmatch/urls.py
+++ skillmatch/skillmatch/urls.py
@@ -21,5 +21,6 @@ from matching import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', views.login, name='login')
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home')
 ]
