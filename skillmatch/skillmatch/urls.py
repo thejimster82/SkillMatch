add611a65b26f15e5a542be22fade032ba856f72
coldbuffalo <coldbuffalo0@gmail.com>
2019-02-13 13:47:21 -0500

    log in page routed, skillmatch/matching/templates/login.html

--- skillmatch/skillmatch/urls.py
+++ skillmatch/skillmatch/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from matching import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('', views.login, name='login')
 ]
