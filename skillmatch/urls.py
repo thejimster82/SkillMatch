05488994f91ef53e2aa8efbb25b2bb8bf9b18a44
Grady Roberts <gnr7aj@virginia.edu>
2019-02-17 23:35:07 -0500

    add home page redirect

--- skillmatch/urls.py
+++ skillmatch/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 from matching import views
 
+
 urlpatterns = [
+    path('/', RedirectView.as_view(url=views.login)),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', views.login, name='login'),
