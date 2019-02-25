4ca558ddd7dcae65ca5057df9a2793fe17fdc44a
unknown <jimmyhwrtn82@gmail.com>
2019-02-25 14:46:17 -0500

    Reconfigured static root and dirs

--- skillmatch/settings.py
+++ skillmatch/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'matching/static/matching')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # django allauth settings
