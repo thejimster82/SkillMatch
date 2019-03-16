d700f73e34923e5eaa16be5d3fbcebc70ee2bd27
Grady Roberts <gnr7aj@virginia.edu>
2019-03-16 14:10:34 -0400

    change profile URL to be profile/ instead of matching/

--- matching/urls.py
+++ matching/urls.py
@@ -3,6 +3,5 @@ from . import views
 from django.contrib.auth import logout, login
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('edit/', views.update_profile, name='update_profile'),
+
 ]
