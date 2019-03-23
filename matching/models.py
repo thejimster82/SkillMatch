6352b8aaa76c29363787c4cc6b988eb4241212f5
Grady Roberts <gnr7aj@virginia.edu>
2019-03-23 17:34:08 -0400

    fix admin text for profile objects

--- matching/models.py
+++ matching/models.py
@@ -16,6 +16,9 @@ class Profile(models.Model):
 #     team = models.ManyToManyField('Team', blank=True)
     matches = models.ManyToManyField('Profile', blank=True)
 
+    def __str__(self):
+        return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
