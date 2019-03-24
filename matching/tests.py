ab49e445e336625a7e3d306f2078c1598f29dd8f
Grady Roberts <gnr7aj@virginia.edu>
2019-03-24 14:07:39 -0400

    add tests for matches

--- matching/tests.py
+++ matching/tests.py
@@ -16,19 +16,19 @@ class CreateUser(TestCase):
         User.objects.create(username="a", email="hehe@virginia.edu",
                             first_name="first", last_name="last")
 
-    def testEmail(self):
+    def test_email(self):
         em = User.objects.get(username="a")
         self.assertEqual(em.email, "hehe@virginia.edu")
 
-    def testUsername(self):
+    def test_username(self):
         em = User.objects.get(username="a")
         self.assertEqual(em.username, "a")
 
-    def testFirstName(self):
+    def test_first_name(self):
         em = User.objects.get(username="a")
         self.assertEqual(em.first_name, "first")
 
-    def testLastName(self):
+    def test_last_name(self):
         em = User.objects.get(username="a")
         self.assertEqual(em.last_name, "last")
 
@@ -44,15 +44,39 @@ class LoginTest(TestCase):
             user=self.user1, provider="google-oauth2", uid="1234")
         self.client = Client()
 
-    def testSocialAuth(self):
+    def test_social_auth(self):
         path = UserSocialAuth.objects.get_social_auth('google-oauth2', '1234')
         self.assertEqual(path, self.server)
 
-    def testAuthorizedUser(self):
+    def test_authorized_user(self):
         self.client.force_login(self.user1)
         response = self.client.get('/matches/')
         self.assertEqual(response.status_code, 200)
 
-    def testUnauthorizedUser(self):
+    def test_unauthorized_user(self):
         response = self.client.get('/matches/')
         self.assertEqual(response.status_code, 302)
+
+
+class MatchesTest(TestCase):
+
+    def setUp(self):
+        u1 = User.objects.create(
+            username="b", email="test1@virginia.edu", first_name="first1", last_name="last1")
+        p1 = Profile.objects.get(user=u1)
+
+        u2 = User.objects.create(
+            username="c", email="test2@virginia.edu", first_name="first2", last_name="last2")
+        p2 = Profile.objects.get(user=u2)
+
+        p1.matches.add(p2)
+
+    def test_has_match(self):
+        u1 = User.objects.get(username='b')
+        p1 = Profile.objects.get(user=u1)
+        self.assertQuerysetEqual(p1.matches.all(), ['<Profile: c>'])
+
+    def test_no_matches(self):
+        u2 = User.objects.get(username="c")
+        p2 = Profile.objects.get(user=u2)
+        self.assertFalse(p2.matches.all().exists())
