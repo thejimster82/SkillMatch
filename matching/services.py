8a11093bdeeed9c23663d4982ec09c8acbb81009
Grady Roberts <gnr7aj@virginia.edu>
2019-04-04 12:52:53 -0400

    delete old courses

--- matching/services.py
+++ matching/services.py
@@ -4,12 +4,16 @@ from .models import Course
 
 
 def update_courses():
+    old_courses = Course.objects.all()
+    if old_courses.exists():
+        old_courses._raw_delete(old_courses.db)
+
     current_terms = ['2019 Spring', '2019 Fall', '2019 Summer']
     url = 'https://api.devhub.virginia.edu/v1/courses'
     r = requests.get(url)
-    courses = r.json()[records]
-    current_courses = []
+    courses = r.json()['records']
 
+    current_courses = []
     for i in range(len(courses)):
         if courses[i][12] in current_terms:
             current_courses.append(Course(course_title=courses[i][4]))
