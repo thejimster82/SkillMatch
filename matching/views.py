1744ddde9241a0f2f369a458b108f39a116b567c
Grady Roberts <gnr7aj@virginia.edu>
2019-04-13 17:52:44 -0400

    fix indent

--- matching/views.py
+++ matching/views.py
@@ -41,8 +41,8 @@ def home(request):
         if 'accept' in request.POST:
             MatchesTable.objects.create(
                 from_user=user, to_user=seconduser, like=True)
-                seconduser.profile.rank += 1
-                seconduser.profile.save()
+            seconduser.profile.rank += 1
+            seconduser.profile.save()
         if 'reject' in request.POST:
             MatchesTable.objects.create(
                 from_user=user, to_user=seconduser, like=False)
